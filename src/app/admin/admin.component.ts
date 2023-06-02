5f50d55650f116c39f4997ef2039ec6da4d21545
Ankit Srivastava <ankitsonu5@gmail.com>
2023-06-02 13:55:28 +0530

    fix sidebar

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+ declare function ak(): any;
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
-export class AdminComponent {
+
+
+export class AdminComponent implements OnInit {
+  ngOnInit(): void {
+    ak ();
+  }
 
 }
+ 
+
